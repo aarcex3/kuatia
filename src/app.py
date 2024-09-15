16c1449b77b2a332e8c1f828f917b450f5d4c613
Agustin Arce <agustinarceprincich@gmail.com>
2024-09-15 16:18:18 -0400

    refactor: factor out the app

---
+++ src/app.py
@@ -0,0 +1,54 @@
+import falcon
+import falcon.asgi
+from falcon.asgi import App
+
+from src.media import SERIALIZERS
+
+
+class InvoiceResource:
+    async def on_get(self, req: falcon.Request, resp: falcon.Response):
+        """Get all invoices"""
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+        resp.text = "Get all invoices"
+
+    async def on_get_invoice(
+        self, req: falcon.Request, resp: falcon.Response, invoice_id: int
+    ):
+        """Get a specific invoice"""
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+        resp.media = {"message": "Get an invoice", "invoice_id": invoice_id}
+
+    async def on_post(self, req: falcon.Request, resp: falcon.Response):
+        """Add a new invoice"""
+        resp.status = falcon.HTTP_201
+        resp.content_type = falcon.MEDIA_JSON
+        resp.text = "Add a new invoice"
+
+    async def on_put_invoice(
+        self, req: falcon.Request, resp: falcon.Response, invoice_id: int
+    ):
+        """Update an existing invoice"""
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+        resp.media = {"message": "Update an invoice", "invoice_id": invoice_id}
+
+    async def on_delete_invoice(
+        self, req: falcon.Request, resp: falcon.Response, invoice_id: int
+    ):
+        """Delete an invoice"""
+        resp.status = falcon.HTTP_204
+        resp.content_type = falcon.MEDIA_JSON
+        resp.media = {"message": "Delete an invoice", "invoice_id": invoice_id}
+
+
+app: App = falcon.asgi.App()
+
+invoice_resource: InvoiceResource = InvoiceResource()
+
+app.add_route("/invoices", invoice_resource)
+app.add_route("/invoices/{invoice_id}", invoice_resource, suffix="invoice")
+
+app.req_options.media_handlers.update(SERIALIZERS)
+app.resp_options.media_handlers.update(SERIALIZERS)
