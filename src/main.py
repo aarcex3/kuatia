d5e71a831e3af2f265b3185069fb42d4d5fcd85b
Agustin Arce <agustinarceprincich@gmail.com>
2024-09-15 14:35:34 -0400

    refactor (main): create api endpoints

--- src/main.py
+++ src/main.py
@@ -1,37 +1,47 @@
-# examples/things_asgi.py
-
 import falcon
 import falcon.asgi
 
 from src.media import SERIALIZERS
 
 
-# Falcon follows the REST architectural style, meaning (among
-# other things) that you think in terms of resources and state
-# transitions, which map to HTTP verbs.
-class ThingsResource:
+class InvoiceResource:
     async def on_get(self, req: falcon.Request, resp: falcon.Response):
-        """Handles GET requests"""
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.content_type = falcon.MEDIA_JSON  # Default is JSON, so override
-        resp.text = (
-            "\nTwo things awe me most, the starry sky "
-            "above me and the moral law within me.\n"
-            "\n"
-            "    ~ Immanuel Kant\n\n"
-        )
+        """Get all invoices"""
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+        resp.text = "Get all invoices"
+
+    async def on_get_invoice(self, req: falcon.Request, resp: falcon.Response):
+        """Get a specific invoice"""
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+        resp.text = "Get an invoice"
+
+    async def on_post(self, req: falcon.Request, resp: falcon.Response):
+        """Add a new invoice"""
+        resp.status = falcon.HTTP_201
+        resp.content_type = falcon.MEDIA_JSON
+        resp.text = "Add a new invoice"
+
+    async def on_put_invoice(self, req: falcon.Request, resp: falcon.Response):
+        """Update an existing invoice"""
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+        resp.text = "Update an invoice"
+
+    async def on_delete_invoice(self, req: falcon.Request, resp: falcon.Response):
+        """Delete an invoice"""
+        resp.status = falcon.HTTP_204
+        resp.content_type = falcon.MEDIA_JSON
+        resp.text = "Delete an invoice"
 
 
-# falcon.asgi.App instances are callable ASGI apps...
-# in larger applications the app is created in a separate file
 app = falcon.asgi.App()
 
-# Resources are represented by long-lived class instances
-things = ThingsResource()
+invoice_resource = InvoiceResource()
 
-# things will handle all requests to the '/things' URL path
-app.add_route("/things", things)
-
+app.add_route("/invoices", invoice_resource)
+app.add_route("/invoices/{invoice_id}", invoice_resource, suffix="invoice")
 
 app.req_options.media_handlers.update(SERIALIZERS)
 app.resp_options.media_handlers.update(SERIALIZERS)
