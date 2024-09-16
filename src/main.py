11c551bfcc1b7bbbe327af45bde0d6f4af935a75
Agustin Arce <agustinarceprincich@gmail.com>
2024-09-16 10:17:45 -0400

    migrate to litestar

--- src/main.py
+++ src/main.py
@@ -2,13 +2,6 @@ import asyncio
 
 import uvicorn
 
-from src.database.manager import db
-
-
-async def initialize_db():
-    await db.setup()
-
-
 if __name__ == "__main__":
-    asyncio.run(initialize_db())
+
     uvicorn.run("src.app:app", host="127.0.0.1", port=8000)
